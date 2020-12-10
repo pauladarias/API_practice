b53c0e03d1061f014f6fba1958fb98286c3e4fa8
pauladarias <pauladarias@gmail.com>
2020-12-10 13:28:31 +0000

    Fetch img from external api

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+// fetch data from local .json file
+
 // fetch("people.json")
-//   .then( response => response.json())
-//   .then ( json => {
-//     const div = document.createElement("div");
-//     div.innerHTML = json.name; 
-//     document.body.appendChild(div); 
+//   .then(response => response.json())
+//   .then(json => {
+//     json.forEach( person => {
+//       const div = document.createElement("div");
+//       div.innerHTML = `${person.name} is ${person.age}`
+//       document.body.appendChild(div); 
+//     })
 //   })
 
-fetch("people.json")
-  .then(response => response.json())
-  .then(json => {
-    json.forEach( person => {
-      const div = document.createElement("div");
-      div.innerHTML = `${person.name} is ${person.age}`
-      document.body.appendChild(div); 
+  // fetch photo from and external API
 
+  fetch ("https://jsonplaceholder.typicode.com/photos/3")
+    .then (res => res.json())
+    .then (json => {
+      console.log(json)
+      const img = document.createElement("img");
+      img.src = json.url
+      document.body.appendChild(img)
     })
-
-  })
