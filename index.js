df73a6a2f144b994ec3a0a32ba85c1843c11089e
pauladarias <pauladarias@gmail.com>
2020-12-10 13:36:28 +0000

    ERROR handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// fetch data from local .json file
+// FETCH DATA FROM LOCAL .JSON FILE  
 
 // fetch("people.json")
 //   .then(response => response.json())
@@ -10,13 +10,26 @@
 //     })
 //   })
 
-  // fetch photo from and external API
+  // FETCH PHOTO FROM EXTERNAL API
 
-  fetch ("https://jsonplaceholder.typicode.com/photos/3")
-    .then (res => res.json())
+  // fetch ("https://jsonplaceholder.typicode.com/photos/3")
+  //   .then (res => res.json())
+  //   .then (json => {
+  //     console.log(json)
+  //     const img = document.createElement("img");
+  //     img.src = json.url
+  //     document.body.appendChild(img)
+  //   })
+
+  // ERROR HANDLING
+
+  fetch("people.json")
+    .then(res => {throw 404} )
     .then (json => {
-      console.log(json)
-      const img = document.createElement("img");
-      img.src = json.url
-      document.body.appendChild(img)
+      json.forEach (person => {
+        const div = createElement("div")
+        div.innerHTML = `${person.name} is ${person.age}`
+        document.body.appendChils(div)
+      })
     })
+    .catch(err => console.error(err))
