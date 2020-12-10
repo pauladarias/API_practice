9955edd2bc98425259719820e54b1a30f72f6404
pauladarias <pauladarias@gmail.com>
2020-12-10 13:18:06 +0000

    First json fetch basic

---
+++ index.js
@@ -0,0 +1,19 @@
+// fetch("people.json")
+//   .then( response => response.json())
+//   .then ( json => {
+//     const div = document.createElement("div");
+//     div.innerHTML = json.name; 
+//     document.body.appendChild(div); 
+//   })
+
+fetch("people.json")
+  .then(response => response.json())
+  .then(json => {
+    json.forEach( person => {
+      const div = document.createElement("div");
+      div.innerHTML = `${person.name} is ${person.age}`
+      document.body.appendChild(div); 
+
+    })
+
+  })
